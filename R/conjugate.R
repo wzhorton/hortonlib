7846869b480789bfff5b2bbcb668f109effbb412
wzhorton <wz.horton@gmail.com>
2018-09-21 14:27:09 -0600

    Conjugate N-N added

--- R/conjugate.R
+++ R/conjugate.R
@@ -24,3 +24,29 @@ update_normal_invgamma <- function(y, a, b, mu, R, R_inv) {
 }
 
 
+#' Conjugate Multivariate Normal - Multivariate Normal Update
+#'
+#' Generates a value from the posterior distribution in the case where
+#' there is a multivariate normal likelihood and a multivariate normal prior.\cr \cr
+#' Argument Model:\cr
+#' y ~ Nn(X*beta, Sig)\cr
+#' beta ~ Np(mu, V)\cr
+#' @param y vector of values at the likelihood level.
+#' @param X fixed design matrix in likelihood.
+#' @param mu prior mean vector.
+#' @param Sig,Sig_inv likelihood covariance/precision matrix.
+#' @param V,V_inv prior covariance/precision matrix.
+#' @export
+
+update_normal_normal <- function(y, X, mu, Sig, V, Sig_inv, V_inv) {
+  if (!xor(missing(Sig), missing(Sig_inv))) {
+    stop("Provide either Sig or Sig_inv, but not both")
+  }
+  if (!xor(missing(V), missing(V_inv))) stop("Provide either V or V_inv, but not both")
+  if (missing(Sig_inv)) Sig_inv <- chol2inv(chol(Sig))
+  if (missing(V_inv)) V_inv <- chol2inv(chol(V))
+
+  vv <- t(X) %*% Sig_inv %*% X + V_inv
+  vterm <- chol2inv(chol(vv))
+  return(rmnorm(vterm %*% (t(X) %*% (Sig_inv %*% y) + V_inv %*% mu), prec = vv))
+}
